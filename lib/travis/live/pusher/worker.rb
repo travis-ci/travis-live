36294fb17c386901832471f847b3044892752466
Piotr Sarnacki <drogus@gmail.com>
2015-06-10 18:09:54 +0200

    Try to name worker class Travis::Async::Sidekiq::Worker

--- lib/travis/live/pusher/worker.rb
+++ lib/travis/live/pusher/worker.rb
@@ -1,17 +1,11 @@
 require 'sidekiq'
 
-module Travis
-  module Live
-    module Pusher
-      class Worker
-        include Sidekiq::Worker
-        sidekiq_options retry: true, queue: 'pusher-live'
+class Travis::Async::Sidekiq::Worker
+  include Sidekiq::Worker
+  sidekiq_options retry: true, queue: 'pusher-live'
 
-        def perform(uuid, target, method, *args)
-          p [:perform, uuid, target, method, args]
-          Travis.logger.info("Perform: " + [:perform, uuid, target, method, args].inspect)
-        end
-      end
-    end
+  def perform(uuid, target, method, *args)
+    p [:perform, uuid, target, method, args]
+    Travis.logger.info("Perform: " + [:perform, uuid, target, method, args].inspect)
   end
 end
