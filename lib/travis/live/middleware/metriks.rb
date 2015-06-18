45e375200515ce9e55c367b68671d1cdca278af4
Josh Kalderimis <josh.kalderimis@gmail.com>
2015-06-18 23:32:29 +0200

    add metrics for the event type

--- lib/travis/live/middleware/metriks.rb
+++ lib/travis/live/middleware/metriks.rb
@@ -8,6 +8,12 @@ module Travis
           begin
             ::Metriks.meter("travis-live.jobs.#{queue}").mark
             ::Metriks.timer("travis-live.jobs.#{queue}.perform").time(&block)
+
+            _, _, _, _, params = *message['args']
+            if event = params['event']
+              event = event.gsub(':', '-')
+              ::Metriks.meter("travis-live.events.#{event}").mark
+            end
           rescue Exception
             ::Metriks.meter("travis-live.jobs.#{queue}.failure").mark
             raise
