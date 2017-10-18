3223c8298ca7eb0a5d2447ec2e9cc04f8e101b93
Piotr Sarnacki <drogus@gmail.com>
2017-10-18 18:09:47 +0200

    Set Sentry using travis-exceptions
    This commit sets Sentry support (as it was broken till now) using
    travis-exceptions gem

--- lib/travis/live/pusher.rb
+++ lib/travis/live/pusher.rb
@@ -1,13 +1,12 @@
 $:.unshift(File.expand_path('../..', File.dirname(__FILE__)))
 
-require 'bundler/setup'
 require 'metriks/librato_metrics_reporter'
-require 'travis/live/error_handler'
 require 'travis/live/config'
 require 'travis/live/pusher/worker'
 require 'travis/live/middleware/metriks'
 require 'travis/live/middleware/logging'
-require 'travis/support/exceptions'
+require 'travis/exceptions'
+require 'travis/exceptions/sidekiq'
 require 'travis/support/logging'
 require 'travis/support/logger'
 require 'travis/support/metrics'
@@ -39,16 +38,6 @@ end
 
 $stdout.sync = true
 
-if Travis.config.sentry.dsn
-  require 'raven'
-  Raven.configure do |config|
-    config.dsn = Travis.config.sentry.dsn
-
-    config.current_environment = Travis.env
-    config.environments = ["staging", "production"]
-  end
-end
-
 Sidekiq.configure_server do |config|
   pro = ::Sidekiq::NAME == 'Sidekiq Pro'
 
@@ -65,16 +54,14 @@ Sidekiq.configure_server do |config|
     chain.add Travis::Live::Middleware::Metriks
     chain.add Travis::Live::Middleware::Logging
 
-    if defined?(::Raven::Sidekiq)
-      chain.remove(::Raven::Sidekiq)
+    if Travis.config.sentry
+      chain.add Travis::Exceptions::Sidekiq
     end
-
-    chain.add(Travis::Live::ErrorHandler)
   end
 end
 
 if Travis.config.sentry
-  Travis::Exceptions::Reporter.start
+  Travis::Exceptions.setup(Travis.config, Travis.config.env, Travis.logger)
 end
 
 Travis::Metrics.setup(Travis.config.metrics.reporter)
